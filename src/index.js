6622025b557b908fc9cb2bed5f91116db687d650
philip du <philipdu@philips-MacBook-Pro.local>
2019-05-18 17:44:57 -0700

    Add comment

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import "./style/reset.css";
 import "./index.css";
 import "./fonts/fonts.css";
 
-
+//testing comments
 ReactDOM.render(
-    <HashRouter>
-      <Main />
-    </HashRouter>,
+  <HashRouter>
+    <Main />
+  </HashRouter>,
   document.getElementById("root")
 );
